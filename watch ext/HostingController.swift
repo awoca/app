228df46667aba82d2b6930bdf8ad9a779118715b
vauxhall <zero.griffin@gmail.com>
2020-04-14 09:09:14 +0200

    Configure project

---
+++ watch ext/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  watch WatchKit Extension
+//
+//  Created by vaux on 14.04.20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
